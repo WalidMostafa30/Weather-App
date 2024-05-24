30666106a3e92717a33df42bc718e1f7c3cb498d
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2024-05-24 22:27:25 +0300

    212

---
+++ src/assets/Data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: "1",
+    title: "To-Do",
+    items: [
+      {
+        content: "ToDo 1",
+        id: "1",
+      },
+      {
+        content: "ToDo 2",
+        id: "2",
+      },
+      {
+        content: "ToDo 3",
+        id: "3",
+      }
+    ],
+  },
+  {
+    id: "2",
+    title: "In-Progress",
+    items: [],
+  },
+  {
+    id: "3",
+    title: "Done",
+    items: [],
+  },
+];
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -2,16 +2,36 @@
 import "./WeatherCard.css";
 import humimg from "../../assets/Images/humidity.svg";
 import windimg from "../../assets/Images/wind.svg";
+import Clouds from "../../assets/Images/icons/Clouds.png";
+import Clear from "../../assets/Images/icons/Clear.png";
+import Drizzle from "../../assets/Images/icons/Drizzle.png";
+import Fog from "../../assets/Images/icons/Fog.png";
+import Rain from "../../assets/Images/icons/Rain.png";
+import Snow from "../../assets/Images/icons/Snow.png";
+import Windy from "../../assets/Images/icons/Windy.png";
 
 const WeatherCard = ({ data }) => {
+  let img;
+  if (data.data.weather[0].main === "Clouds") {
+    img = Clouds;
+  } else if (data.data.weather[0].main === "Clear") {
+    img = Clear;
+  } else if (data.data.weather[0].main === "Drizzle") {
+    img = Drizzle;
+  } else if (data.data.weather[0].main === "Fog") {
+    img = Fog;
+  } else if (data.data.weather[0].main === "Rain") {
+    img = Rain;
+  } else if (data.data.weather[0].main === "Snow") {
+    img = Snow;
+  } else if (data.data.weather[0].main === "Windy") {
+    img = Windy;
+  }
+
   return (
     <div className="WeatherCard">
       <h1 className="WeatherCard__title">{data.data.name}</h1>
-      <img
-        className="WeatherCard__img"
-        src={`/public/icons/${data.data.weather[0].main}.png`}
-        alt="img"
-      />
+      <img className="WeatherCard__img" src={img} alt="img" />
 
       <div className="WeatherCard__temp">
         <h1>
